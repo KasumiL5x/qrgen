5d33cd654395997549059412a076fc2659932cff
Daniel <KasumiL5x@gmail.com>
2020-07-31 21:11:49 +0100

    Indentation change (silly vim!).

--- app.py
+++ app.py
@@ -17,55 +17,55 @@ FMT_ARG = 'fmt'
 
 @app.route('/')
 def index():
-    # Get text.
-    txt = get_url_arg(request, TXT_ARG)
-    if txt is None:
-        return jsonify(error=f"Error: Missing argument '{TXT_ARG}'."), 400
-    # Verify text.
-    if not len(txt):
-        return jsonify(error=f'Error: Text cannot be empty.'), 400
+  # Get text.
+  txt = get_url_arg(request, TXT_ARG)
+  if txt is None:
+    return jsonify(error=f"Error: Missing argument '{TXT_ARG}'."), 400
+  # Verify text.
+  if not len(txt):
+    return jsonify(error=f'Error: Text cannot be empty.'), 400
 
-    # Get format.
-    fmt = get_url_arg(request, FMT_ARG)
-    if fmt is None:
-        return jsonify(error=f"Error: Missing argument '{FMT_ARG}'."), 400
-    # Verify format.
-    FMT_OPTS = [FMT_ASCII, FMT_SVG, FMT_PNG]
-    if fmt not in FMT_OPTS:
-        available_opts = ', '.join(FMT_OPTS)
-        return jsonify(error=f'Error: Invalid format. Valid types are: {available_opts}.'), 400
+  # Get format.
+  fmt = get_url_arg(request, FMT_ARG)
+  if fmt is None:
+    return jsonify(error=f"Error: Missing argument '{FMT_ARG}'."), 400
+  # Verify format.
+  FMT_OPTS = [FMT_ASCII, FMT_SVG, FMT_PNG]
+  if fmt not in FMT_OPTS:
+    available_opts = ', '.join(FMT_OPTS)
+    return jsonify(error=f'Error: Invalid format. Valid types are: {available_opts}.'), 400
 
-    return make_qr(txt, fmt)
+  return make_qr(txt, fmt)
 
 def get_url_arg(request, name):
-    return None if name not in request.args else request.args[name]
+  return None if name not in request.args else request.args[name]
 
 def make_qr(txt, fmt):
-    qr = qrcode.QRCode()
-    qr.add_data(txt)
-    qr.make(fit=True)
+  qr = qrcode.QRCode()
+  qr.add_data(txt)
+  qr.make(fit=True)
 
-    if fmt == FMT_ASCII:
-        sio = StringIO()
-        qr.print_ascii(out=sio)
-        return jsonify(image=sio.getvalue())
+  if fmt == FMT_ASCII:
+    sio = StringIO()
+    qr.print_ascii(out=sio)
+    return jsonify(image=sio.getvalue())
 
-    if fmt == FMT_SVG:
-        img = qr.make_image(image_factory=qrcode.image.svg.SvgImage)
-        buff = BytesIO()
-        img.save(buff)
-        img_str = bytes("data:image/svg+xml;base64,", encoding='utf-8') + base64.b64encode(buff.getvalue())
-        return jsonify(image=img_str.decode('utf-8'))
+  if fmt == FMT_SVG:
+    img = qr.make_image(image_factory=qrcode.image.svg.SvgImage)
+    buff = BytesIO()
+    img.save(buff)
+    img_str = bytes("data:image/svg+xml;base64,", encoding='utf-8') + base64.b64encode(buff.getvalue())
+    return jsonify(image=img_str.decode('utf-8'))
 
-    if fmt == FMT_PNG:
-        img = qr.make_image()
-        buff = BytesIO()
-        img.save(buff, format='png')
-        img_str = bytes("data:image/jpeg;base64,", encoding='utf-8') + base64.b64encode(buff.getvalue())
-        return jsonify(image=img_str.decode('utf-8'))
+  if fmt == FMT_PNG:
+    img = qr.make_image()
+    buff = BytesIO()
+    img.save(buff, format='png')
+    img_str = bytes("data:image/jpeg;base64,", encoding='utf-8') + base64.b64encode(buff.getvalue())
+    return jsonify(image=img_str.decode('utf-8'))
 
-    # This should never happen as the type is checked elsewhere, but just in case it changes.
-    return jsonify(error='Error: Unrecognized format.'), 400
+  # This should never happen as the type is checked elsewhere, but just in case it changes.
+  return jsonify(error='Error: Unrecognized format.'), 400
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
